03d3d329cb1168acd5b8858ef19465ea35c346e7
Jeroen Honig <jeroen@Mac.localdomain>
2025-04-30 15:20:29 +0200

    Verbeter setup.sh: initdb via su, betere IP-detectie, npm/pm2 fixes

--- install/lxc/setup.sh
+++ install/lxc/setup.sh
@@ -1,73 +1,77 @@
-#!/bin/sh
-# Verbeterde setup.sh voor BV Leningen App op Alpine Linux
-set -euxo pipefail
+#!/bin/ash
+set -eux
 
-# 1. Basis packages installeren
+echo "BV Leningen App installatie gestart op $(date)"
+
+echo "1. Basissysteem bijwerken..."
 apk update
-apk add --no-cache git curl bash nodejs npm postgresql17 postgresql17-contrib postgresql17-openrc nginx nginx-openrc dcron dcron-openrc
-npm install -g pm2
+apk upgrade
 
-# 2. Clone applicatie
-mkdir -p /opt/leningen-app
-cd /opt/leningen-app
-git clone https://github.com/jeroenhonig/bv-leningen-app repo
+echo "2. PostgreSQL installeren..."
+apk add postgresql17 postgresql17-contrib postgresql17-openrc postgresql-common postgresql-common-openrc
 
-# 3. Database initialiseren
-su - postgres -c "/usr/bin/initdb -D /var/lib/postgresql/17/data"
+su - postgres -c '/usr/bin/initdb -D /var/lib/postgresql/17/data'
 rc-update add postgresql
 rc-service postgresql start
 
-# 4. Database configureren
+echo "3. Node.js installeren..."
+apk add nodejs npm
+
+echo "4. Nginx installeren..."
+apk add nginx nginx-openrc
+rc-update add nginx
+
+echo "5. Applicatie clonen van GitHub..."
+apk add git
+mkdir -p /opt/leningen-app
+cd /opt/leningen-app
+git clone https://github.com/jeroenhonig/bv-leningen-app.git repo
+
+echo "6. Database configureren..."
 psql -U postgres <<EOF
-CREATE USER leningen_user WITH PASSWORD 'leningen_pass';
+CREATE ROLE leningen_user WITH LOGIN PASSWORD 'leningen_pass';
 CREATE DATABASE leningen_db OWNER leningen_user;
 EOF
+
 psql -U postgres -d leningen_db < /opt/leningen-app/repo/database/schema.sql
 
-# 5. Backend installeren
+echo "7. Backend installeren..."
 cd /opt/leningen-app/repo/backend
 npm install
-pm run build
 
-# 6. Frontend bouwen
+echo "8. Frontend bouwen..."
 cd /opt/leningen-app/repo/frontend
 npm install
 npm run build
 
-# 7. Nginx configureren
-mkdir -p /var/www/html
-cp -r /opt/leningen-app/repo/frontend/build/* /var/www/html/
-rc-update add nginx
-rc-service nginx start
+echo "9. Nginx configureren..."
+rm -rf /var/www/localhost/htdocs/*
+cp -r /opt/leningen-app/repo/frontend/build/* /var/www/localhost/htdocs/
 
-# 8. Backend met PM2 starten
-cd /opt/leningen-app/repo/backend
-pm install
-pm run build
-pm2 start dist/index.js --name leningen-app
-pm2 save
-
-# 9. PM2 autostart
-pm install pm2 -g
-pm2 startup | sh
-
-# 10. Cron toevoegen
+echo "10. Backup configureren..."
+apk add dcron dcron-openrc
 rc-update add dcron
 rc-service dcron start
 
-# 11. PostgreSQL optimalisatie voor kleine container
-POSTGRES_CONF="/var/lib/postgresql/17/data/postgresql.conf"
-if [ -f "$POSTGRES_CONF" ]; then
-  echo "shared_buffers = 64MB" >> "$POSTGRES_CONF"
-  echo "work_mem = 1MB" >> "$POSTGRES_CONF"
-  rc-service postgresql restart
-fi
+echo "11. Services starten..."
+npm install -g pm2
+cd /opt/leningen-app/repo/backend
+pm2 start index.js --name leningen-backend
+pm2 save
+rc-service nginx start
 
-# 12. Installatie voltooid
+echo "12. PostgreSQL optimaliseren voor kleine container..."
+echo "shared_buffers = 64MB" >> /var/lib/postgresql/17/data/postgresql.conf
+echo "work_mem = 4MB" >> /var/lib/postgresql/17/data/postgresql.conf
+rc-service postgresql restart
+
 echo "=============================================="
 echo "BV Leningen App Installatie Voltooid!"
+echo "=============================================="
 echo "Database naam: leningen_db"
 echo "Database gebruiker: leningen_user"
 echo "Database wachtwoord: leningen_pass"
-echo "Web URL: http://\$(hostname -i)"
+echo "Web URL: http://$(ip -4 addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f1)"
+echo ""
+echo "Deze gegevens zijn opgeslagen in: /root/leningen-app-install.log"
 echo "=============================================="
